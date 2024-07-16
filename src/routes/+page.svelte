f93efbf103b5d199eb89f72f7306d1a426fce1cc
Daniel Tran <129140506+DanielvTran@users.noreply.github.com>
2024-07-16 10:28:57 +1000

    feat: footer added + change cron job time

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,10 +3,12 @@
 	import Nav from '../components/Nav.svelte';
 	import Hero from '../components/Hero.svelte';
 	import About from '../components/About.svelte';
+	import Footer from '../components/Footer.svelte';
 </script>
 
 <div class="flex flex-col w-full">
 	<Nav />
 	<Hero />
 	<About />
+	<Footer />
 </div>
